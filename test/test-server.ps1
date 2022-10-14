93250e81f1ba3fca298f54672dfa9609c979f77c
Spyros Desyllas <s.desyllas@kaizengaming.com>
2022-10-14 23:19:12 +0300

    test(app): implement named pipe powershell test script

---
+++ test/test-server.ps1
@@ -0,0 +1,32 @@
+﻿try
+{
+    $server = new-object System.IO.Pipes.NamedPipeServerStream('test-server',
+        [System.IO.Pipes.PipeDirection]::InOut)
+
+    'Waiting for client connection'
+    $server.WaitForConnection()
+    'Connection established'
+
+    $pipeReader = new-object System.IO.StreamReader($server)
+    $script:pipeWriter = new-object System.IO.StreamWriter($server)
+    $pipeWriter.AutoFlush = $true
+
+    while ($true) {
+        $what = Read-Host "What"
+        "Asking $what"
+        $pipeWriter.WriteLine($What)
+        $ans = $pipeReader.ReadLine()
+        "The answer is $ans"
+    }
+}
+catch {
+    $_
+}
+finally
+{
+    'Exiting'
+    if ($server -ne $null)
+    {
+        $server.Dispose()
+    }
+}
